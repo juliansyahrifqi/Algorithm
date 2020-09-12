55999f5b368102c03b8d49a7e12a80a79be47a8d
juliansyahrifqi <rifqipratama9@gmail.com>
2020-09-13 00:53:48 +0700

    add bubble sort algorithm

---
+++ Sorting Algorithm/bubblesort.js
@@ -0,0 +1,30 @@
+// Sorting algorithm ( bubble sort ) 
+let bubblesort = (arr) => {
+    const length = arr.length;
+
+    for(let i = 0; i < length; i++) {
+        for(let j = 0; j < length; j++) {
+            if(arr[j] > arr[j+1]) {
+                const temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }    
+        }
+    }
+    return arr;
+}
+
+// Randomize array function
+let randoms = (n) => {
+    let random = [...Array(n)].map(() => Math.floor(Math.random() * (n * 5)));
+
+    return random;
+}
+
+const randomArr = randoms(20);
+console.log("Unsorted array: ");
+console.log(randomArr.join(' ') + "\n");
+
+console.log("Sorted array: ");
+console.log(bubblesort(randomArr).join(' '));
+
