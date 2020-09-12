4d8e8cee12e2440bf2126bdfafe2be248d79f3a7
juliansyahrifqi <rifqipratama9@gmail.com>
2020-09-13 00:55:52 +0700

    add quick sort algorithm

---
+++ Sorting Algorithm/quicksort.js
@@ -0,0 +1,43 @@
+function quicksort(data, start, end) {
+	function partisi(data, start, end) {
+		function swap(x, y) {
+			const temp = data[y];
+			data[y] = data[x];
+			data[x] = temp;
+		}
+
+		const pivot = data[end];
+		let center = start;
+
+		for(let i = start; i < end; ++i) {
+			if(data[i] < pivot) {
+				swap(center, i);
+				++center;
+			}
+		}
+		swap(center, end);
+		return center;
+	}
+
+	if(start >= end) return;
+
+	const center = partisi(data, start, end);
+	quicksort(data, start, center - 1);
+	quicksort(data, center + 1, end);
+}
+
+// Randomize array function
+let randomize = (n) => {
+    let random = [...Array(n)].map(() => Math.floor(Math.random() * (n+5)));
+
+    return random;
+}
+
+const randomArr = randomize(10);
+
+console.log("Unsorted: ");
+console.log(randomArr.join(' ') + "\n");
+quicksort(randomArr, 0, randomArr.length -1);
+
+console.log("Sorted: ");
+console.log(randomArr.join(' '));
